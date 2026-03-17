a403f69f6499c44bc96c00a9abd59e03957957b4
João Victor Figueiredo <126639756+joaovictorfigueiredo@users.noreply.github.com>
2026-03-17 10:34:44 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import { NavegacaoPools } from "./components/NavegacaoPools";
 import ContadorRegressivo from "./components/ContadorRegressivo";
 import { FunctionsHttpError } from '@supabase/supabase-js';
 import { Turnstile } from '@marsidev/react-turnstile';
-import AtualizarSenha from './atualizar-senha';
+
 
 
 
@@ -29,7 +29,7 @@ type AbaType = 'explorar' | 'minhas_apostas' | 'criadas_por_mim';
 
 
 function App() {
-  const currentPath = window.location.pathname;
+  
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState<any>(null)
@@ -951,10 +951,6 @@ async function handleResetPassword() {
   const poolsFiltradas = filtroAtivo === 'Todos'
     ? [...pools].sort((a, b) => calcularDadosPool(b).totalPote - calcularDadosPool(a).totalPote)
     : pools.filter(p => p.category === filtroAtivo)
-  
-if (currentPath === '/atualizar-senha') {
-  return <AtualizarSenha />;
-}
   
  if (!user) {
   return (
