d05a97c8aab2c17bd31088a270d30fb5a4adea33
João Victor Figueiredo <126639756+joaovictorfigueiredo@users.noreply.github.com>
2026-03-17 14:01:21 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Toaster } from 'react-hot-toast'; // 1. Importa o motor dos alertas
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    {/* 2. Adiciona o Toaster aqui para os alertas aparecerem no topo */}
+    <Toaster position="top-center" reverseOrder={false} />
     <App />
   </StrictMode>,
 )
