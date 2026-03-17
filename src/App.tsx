e4e3c14ab9d82f1deb33cfa36e0b0f440bc309d4
João Victor Figueiredo <126639756+joaovictorfigueiredo@users.noreply.github.com>
2026-03-17 10:07:57 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -27,18 +27,6 @@ type AbaType = 'explorar' | 'minhas_apostas' | 'criadas_por_mim';
 
 
 function App() {
-  const currentPath = window.location.pathname;
-
-  // ESTA DEVE SER A PRIMEIRA COISA NO RETURN
-  if (currentPath === '/atualizar-senha') {
-    return <AtualizarSenha />;
-  }
-
-  // Só depois vem a lógica de autenticação/login
-  return (
-    // ... resto do seu código
-  );
-}
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState<any>(null)
@@ -116,7 +104,18 @@ const [modo, setModo] = useState<'login' | 'recuperar'>('login');
 
 const [isModalRankingOpen, setIsModalRankingOpen] = useState(false);
 const [isCarteiraMobileAberta, setIsCarteiraMobileAberta] = useState(false);
+const currentPath = window.location.pathname;
 
+  // ESTA DEVE SER A PRIMEIRA COISA NO RETURN
+  if (currentPath === '/atualizar-senha') {
+    return <AtualizarSenha />;
+  }
+
+  // Só depois vem a lógica de autenticação/login
+  return (
+    // ... resto do seu código
+  );
+}
 
 const [denunciaInfo, setDenunciaInfo] = useState<{aberto: boolean, poolId: string} | null>(null);
 const dispararDenuncia = (poolId: string) => {
