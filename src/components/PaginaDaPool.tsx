87764e465bb6962d3a51322506bd012276dd17e2
João Victor Figueiredo <126639756+joaovictorfigueiredo@users.noreply.github.com>
2026-03-19 15:52:54 -0300

    Update PaginaDaPool.tsx

--- src/components/PaginaDaPool.tsx
+++ src/components/PaginaDaPool.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 import ContadorRegressivo from "./ContadorRegressivo";
-import { supabase } from './lib/supabase
+import { supabase } from '../lib/supabase';
 
 type Props = {
   pool: any
