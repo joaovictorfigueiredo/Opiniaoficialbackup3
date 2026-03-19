1b38e13b4699b4e96ce4c5e2800ee4d0ea05be78
João Victor Figueiredo <126639756+joaovictorfigueiredo@users.noreply.github.com>
2026-03-19 16:22:46 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ import { FunctionsHttpError } from '@supabase/supabase-js';
 import { Turnstile } from '@marsidev/react-turnstile';
 import AtualizarSenha from './atualizar-senha';
 import { toast } from 'react-hot-toast';
-import PaginaDaPool from './components/PaginaDaPool';
+
 
 
 // --- TIPOS ---
